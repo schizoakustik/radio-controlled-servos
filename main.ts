bf94557487b3e2d839fbee78fab4342f1e539816
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2022-09-20 11:35:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        angle += -1
+    } else if (receivedNumber == 2) {
+        angle += 1
+    } else {
+        angle = 90
+    }
+    servos.P0.setAngle(angle)
+})
+let angle = 0
+radio.setGroup(1)
+angle = 90
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+    }
+    if (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+        radio.sendNumber(0)
+    }
+})
